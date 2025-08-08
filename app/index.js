30b8d83411d35f4846a8233df8cb0251398fd782
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2025-08-08 00:35:51 -0700

    added fun date to default route

--- app/index.js
+++ app/index.js
@@ -4,8 +4,20 @@
     const port = process.env.PORT || 3000;
 
     app.get('/', (req, res) => {
-        res.send('Hello World!');
+        res.send(`Hello World!\nGuess what day ${genRandomDate()} is!`);
     });
+
+    function genRandomDate() {
+        const start = new Date(1582, 10, 15); // Start date: October 15, 1582, when the Gregorian calendar was adopted
+        const temp_now = new Date();
+
+        // Create a date as far into the future as the adoption of the Gregorian calendar was in the past
+        const end = new Date(temp_now.getTime() + (temp_now.getTime() - start.getTime()));
+        const randTimeStamp = Math.random() * (end.getTime() - start.getTime());
+        const randomDate = new Date(randTimeStamp);
+
+        return randomDate.toLocaleDateString('en-US');
+    }
 
     app.listen(port, () => {
         console.log(`Server running on port ${port}`);
