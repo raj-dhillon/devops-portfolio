b62dd9e0c50ffbcee494444fe97fef9b35f7b14d
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2025-08-09 02:05:44 -0700

    added skeleton layout for phaserjs

--- app/index.js
+++ app/index.js
@@ -2,6 +2,10 @@
     const express = require('express');
     const app = express();
     const port = process.env.PORT || 3000;
+    const path = require('path');
+
+    app.use(express.static(path.join(__dirname, 'public')));
+
 
     app.get('/', (req, res) => {
         res.send(`<h>Hello World!</h><p>Guess what day <b>${genRandomDate()}</b> is!</p>`);
